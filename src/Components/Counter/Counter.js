b597a049e6932e1d46e5a25cd80c31c68a865420
real7alireza <real7alireza@gmail.com>
2021-02-05 02:37:15 +0330

    getting style better

--- src/Components/Counter/Counter.js
+++ src/Components/Counter/Counter.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './Counter.module.css'
 import Button from './../UI/Button/Button'
 
 const Counter = () => {
+    const [counter, setCounter] = useState(0)
+
+    const increase = () => {
+        setCounter(counter + 1)
+    }
+    const decrease = () => {
+        setCounter(counter - 1)
+    }
+
     return (
         <div className={classes.Counter}>
-            <p>counter</p>
-            <p>number</p>
-            <Button btnType='success'>+</Button>
-            <Button btnType='danger'>-</Button>
+            {/* <h1>counter</h1> */}
+            <div className={classes.CounterElements}>
+                <Button btnType={['danger', 'left']} clicked={decrease}>-</Button>
+                <p className={classes.Number}>{counter}</p>
+                <Button btnType={['success', 'right']} clicked={increase}>+</Button>
+            </div>
         </div>
     )
 }
--- src/Components/UI/Button/Button.js
+++ src/Components/UI/Button/Button.js
@@ -2,9 +2,13 @@ import React from 'react'
 import classes from './Button.module.css'
 
 const Button = (props) => {
-    return(
-        <button onClick={props.clicked} className={[classes.Button, classes[props.btnType]].join(' ')}>{props.children}</button>
+    return (
+        <button
+            onClick={props.clicked}
+            className={[classes.Button, props.btnType.map(cls => classes[cls]).join(' ')].join(' ')}
+        >{props.children}</button>
     )
 }
+
 
 export default Button
