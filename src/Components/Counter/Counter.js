64bb277873d2e09d64ba5e5eb465eab3a8096e4d
real7alireza <real7alireza@gmail.com>
2021-02-04 18:26:32 +0330

    create Counter Component

---
+++ src/Components/Counter/Counter.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import classes from 'Counter.module.css'
+
+const Counter = () => {
+    return (
+        <div className={classes.Counter}>
+            <p>counter</p>
+            <p>number</p>
+            <p>increase</p>
+            <p>decrease</p>
+        </div>
+    )
+}
+
+
+export default Counter
